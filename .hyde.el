271487fe6c1d3c8ec867b912a06d7bafbfbc0a5f
Michael Kohl <citizen428@gmail.com>
2014-10-07 12:37:26 +0700

    Cleanup

--- .hyde.el
+++
@@ -1,5 +0,0 @@
-(setq hyde-posts-dir "source/_posts"
-      hyde-drafts-dir "source/_drafts"
-      hyde/deploy-command "bundle exec rake deploy"
-      hyde/jekyll-command "bundle exec rake generate"
-      hyde/use-octopress t)
